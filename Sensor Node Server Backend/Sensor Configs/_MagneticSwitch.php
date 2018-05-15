7b783f482a3d463b9b91590adcb6cf7e9e036a61
Phil Canete <philcanete1@gmail.com>
2018-05-15 03:02:44 -0700

    Fixed ignoring privateparts

---
+++ Sensor Node Server Backend/Sensor Configs/_MagneticSwitch.php
@@ -0,0 +1,24 @@
+<?php
+
+function SW($data, $table)
+{
+
+    //Set olumn names, parse and set values based on semicolon delimiter
+    $columns = array("State");
+
+
+    try {
+        //Currently error only throws if no data was sent
+        if (!$data) {
+            throw new Exception('Error with ' . basename(__FILE__, '.php') . ' data.');
+        } else {
+            return array($table, $columns, [$data]);
+        }
+    }
+
+        //Add error to database
+    catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n\n";
+        return ['Error Log', ['Message'], [$e->getMessage()]];
+    }
+}
